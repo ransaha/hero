0bfae93eba29614f3d1bd87a4507f17f67b23f36
ransaha <meetranajoysaha@gmail.com>
2017-06-22 09:33:21 +0530

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,16 +1,26 @@
-from django.conf.urls import include, url
-
+from django.conf.urls import url
 from django.contrib import admin
+from hello import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admission$', views.admission, name='admission'),
+    url(r'^$', views.home, name='home'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^it$', views.it, name='it'),
+    url(r'^math$', views.math, name='math'),
+    url(r'^bank$', views.bank, name='bank'),
+    url(r'^new$', views.new, name='new'),
+    url(r'^success$', views.success, name='success'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^db', views.db, name='db'),
 ]
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
