1605701ca19072f3179d7e6dc20e9b7bf8b153a5
ransaha <meetranajoysaha@gmail.com>
2017-06-22 20:59:32 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -66,10 +66,10 @@ def login(request):
                 with connection.cursor() as cursor:
                     cursor.execute('''SELECT email FROM users WHERE username = 'ransaha'  ''')
                     row = cursor.fetchone()
-                    cmd= 'python manage.py syncdb'
-                    out = os.popen(cmd)
-                #request.session['user'] = user
-                return HttpResponse(out.read())
+                    #cmd= 'python manage.py syncdb'
+                    #out = os.popen(cmd)
+                request.session['user'] = user
+                return HttpResponse(row)
                 #return render(request, 'user.html', {'user': row})
 
 def home(request):
