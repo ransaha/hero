ed0354b82039204757528f778a2504f290d1a09c
ransaha <meetranajoysaha@gmail.com>
2017-06-22 22:25:11 +0530

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions.backends.signed_cookies',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello'
