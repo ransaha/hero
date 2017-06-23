fa7731f3fc248b5d5a4da69056251e9357433742
ransaha <meetranajoysaha@gmail.com>
2017-06-23 12:22:55 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -108,8 +108,10 @@ def doubt(request):
             doubt =  request.POST.get('doubt')
             doubt1 = ''' <div class="sc">
 <img src="/static/user2.png" class="img-circle" style="width:80px;height:80px;top:-50px;left:-50px;position:relative;"> %s </div>  ''' %s (doubt)
-            fo = open("/hello/templates/doubt.txt", "a+")
-            fo.write(doubt1)
-            fo.close()
+            dd = os.popen("pwd")
+            return HttpResponse(dd.read())
+            #fo = open("/hello/templates/doubt.txt", "a+")
+            #fo.write(doubt1)
+            #fo.close()
         return render(request, 'doubt.html', {"user" : user})
 
