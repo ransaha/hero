6ea4b701c4f2d63217bc5833da059b773f3cfdef
ransaha <meetranajoysaha@gmail.com>
2017-06-23 12:37:20 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -108,7 +108,7 @@ def doubt(request):
             doubt =  request.POST.get('doubt')
             doubt1 = ''' <div class="sc">
 <img src="/static/user2.png" class="img-circle" style="width:80px;height:80px;top:-50px;left:-50px;position:relative;"> %s </div>  ''' % (doubt)
-            dd = os.popen("ls | grep hello/templates/")
+            dd = os.popen("ls -l")
             return HttpResponse(dd.read())
             #fo = open("/hello/templates/doubt.txt", "a+")
             #fo.write(doubt1)
