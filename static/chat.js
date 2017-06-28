f93bd319aa581aa715b0f04f700b5fa12f8301a0
ransaha <meetranajoysaha@gmail.com>
2017-06-28 23:02:04 +0530

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -13,7 +13,24 @@ $('#chat-form').on('submit', function(event){
     });
 });
 
+function getMessages(){
+    if (!scrolling) {
+        $.get('/message', function(messages){
+            $('#msg-list').html(messages);
+            var chatlist = document.getElementById('msg-list-div');
+            chatlist.scrollTop = chatlist.scrollHeight;
+        });
+    }
+    scrolling = false;
+}
 
+var scrolling = false;
+$(function(){
+    $('#msg-list-div').on('scroll', function(){
+        scrolling = true;
+    });
+    refreshTimer = setInterval(getMessages, 500);
+});
 
 // using jQuery
 function getCookie(name) {
